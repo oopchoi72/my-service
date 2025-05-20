b9ab8aef217e3dd4807eb4a8375e1f9585fb5429
oopchoi72 <148016795+oopchoi72@users.noreply.github.com>
2025-05-21 02:03:56 +0900

    common 모듈에 ConfirmDialog 컴포넌트 추가

--- my-webapp/src/components/common/index.ts
+++ my-webapp/src/components/common/index.ts
@@ -4,3 +4,4 @@ export { default as Spinner } from "./Spinner";
 export { default as ErrorMessage } from "./ErrorMessage";
 export { default as QueryErrorBoundary } from "./QueryErrorBoundary";
 export { default as RetryIndicator } from "./RetryIndicator";
+export { default as ConfirmDialog } from "./ConfirmDialog";
